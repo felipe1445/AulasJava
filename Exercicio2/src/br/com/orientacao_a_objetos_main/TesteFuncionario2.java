a2151ef2028f1788f6b5794ad83c11102de724bd
noturno <fel_dav@hotmail.com>
2019-06-26 18:25:05 -0300

    hotfix

--- Exercicio2/src/br/com/orientacao_a_objetos_main/TesteFuncionario2.java
+++ Exercicio2/src/br/com/orientacao_a_objetos_main/TesteFuncionario2.java
@@ -7,23 +7,20 @@ public class TesteFuncionario2 {
 
 	public static void main(String[] args) {
 		Funcionario funcionario1 = new Funcionario();
-		boolean verifica =true;
+		boolean verifica = true;
+
 		funcionario1.nomeFunc = Teclado.lerTexto("digite o nome: ");
-		
-		while(verifica) {
+		while (verifica) {
 			int auxiliar = Teclado.lerInt("o que vc quer fazer: consultar(1) alterar(2) sair(3)");
-			if (auxiliar==1) {
-				System.out.println(funcionario1.consultaDados());				
-			}else if (auxiliar==2) {				
-					funcionario1.nomeFunc = Teclado.lerTexto("digite o nome: ");
-					funcionario1.salarioFunc = Teclado.lerDouble("digite o salario: ");
+			if (auxiliar == 1) {
+				System.out.println(funcionario1.consultaDados());
+			} else if (auxiliar == 2) {
+				funcionario1.nomeFunc = Teclado.lerTexto("digite o nome: ");
+				funcionario1.salarioFunc = Teclado.lerDouble("digite o salario: ");
+			} else if (auxiliar == 3) {
+				verifica = false;
 			}
-			else if (auxiliar==3) {
-					verifica=false;
-			}					
-		}					
-			
+		}
+
 	}
 }
-
-
